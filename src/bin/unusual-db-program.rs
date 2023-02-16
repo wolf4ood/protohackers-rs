8d81aacb065180ee51374a820c6a6084f7d46083
Enrico Risa <enrico.risa@gmail.com>
2023-02-16 19:40:51 +0100

    wip

--- src/bin/unusual-db-program.rs
+++ src/bin/unusual-db-program.rs
@@ -5,23 +5,30 @@ use tracing::info;
 
 static EMPTY: String = String::new();
 
+static VERSION: &str = "My Version";
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
-    info!("Starting Unusual Database service on 8000 port");
+    info!("Starting Unusual Database service on 8001 port");
 
-    let socket = UdpSocket::bind("localhost:8000").await?;
+    let socket = UdpSocket::bind("0.0.0.0:8001").await?;
 
     let mut store = HashMap::new();
+
+    store.insert("version".to_string(), VERSION.to_string());
     loop {
         let mut buff = [0; 1000];
 
         let (read, addr) = socket.recv_from(&mut buff).await?;
 
+        info!("Received packet");
+
         let msg = std::str::from_utf8(&buff[0..read])?;
 
         match msg.split_once("=") {
+            Some(("version", _)) => {}
             Some((prefix, suffix)) => {
                 store.insert(prefix.to_string(), suffix.to_string());
             }
